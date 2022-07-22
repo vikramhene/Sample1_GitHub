84b1274e257f0d097be469fe1408d6ff2e0a3552
Vinayak Hene <Vinayak Hene@DESKTOP-87HE5SR>
2022-07-22 18:30:12 +0530

    [Vikram]: added new project

---
+++ src/Example1/S1E1.java
@@ -0,0 +1,17 @@
+package Example1;
+
+public class S1E1 {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hi");
+		
+		System.out.println("WHATSSUP");
+		
+		System.out.println("Hello");
+		
+		System.out.println("hi");
+		
+	}
+
+}
